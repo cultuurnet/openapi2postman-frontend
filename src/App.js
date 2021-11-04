330afc1780031e855da44bc9d61d70c65b4765f0
bram <bram.pauwelyn@publiq.be>
2021-11-04 11:43:35 +0100

    Improve layout of page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./index.css";
 import { Form } from "./Form";
+import styled  from "styled-components"
 
 export default function App() {
+
+  const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  flex-wrap: wrap;
+  `;
+
   return (
-    <div className="App">
-      <h1>openapi2postmanv2</h1>
+    <Container className="App">
+      <h1>openapi 2 postman</h1>
       <Form />
-    </div>
+    </Container>
   );
 }
