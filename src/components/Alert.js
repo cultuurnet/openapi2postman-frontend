7db910977a0a9b0db37828e7827b95fae0e41a39
bram <bram.pauwelyn@publiq.be>
2022-01-12 14:00:38 +0100

    API-22 - Add styling for danger alert

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -2,6 +2,11 @@ import styled  from "styled-components"
 
 const Alert = (props) => {
   const Container = styled.div`
+    padding: 15px;
+    background-color: #f2dede;
+    border: 1px solid #ebccd1;
+    color: #a94442;
+    margin-bottom: 2rem;
   `
  return (
   <Container>{props.text}</Container>
