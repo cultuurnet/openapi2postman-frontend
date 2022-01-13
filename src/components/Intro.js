8d0afd0674fed40ad47e7ad869639e9dacc44628
Bert Ramakers <bertramakers@me.com>
2022-01-13 14:47:41 +0100

    Add intro copy

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -4,21 +4,21 @@ const Intro = (props) => {
   const Container = styled.div`
     max-width: 500px;
     background-color: #efefef;
-    padding: 15px;
+    padding: 15px 15px 0 15px;
     border-radius: 5px;
     margin-bottom: 2.5rem;
   `;
 
   const Paragraph = styled.p`
-    margin-bottom: 0;
+    margin-bottom: 15px;
   `;
   return (
     <Container>
       <Paragraph>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat.
+        Ready to start playing around with our APIs? Use the form below to download a personalized collection to import into Postman and start making API requests in a matter of seconds!
+      </Paragraph>
+      <Paragraph>
+        We also provide extensive <a href={'https://docs.publiq.be'}>API documentation</a> with how-to guides, a technical reference for each API endpoint, and info about authentication and error handling.
       </Paragraph>
     </Container>
   );
