0e97e22b524ae264a3fe5c727c88d3a7e209aaf8
bram <bram.pauwelyn@publiq.be>
2022-01-14 16:48:22 +0100

    API-35 - Use styled-components for global styles instead of global.css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './index.css';
 import { Form } from './Form';
 import styled from 'styled-components';
 import { createGlobalStyle } from 'styled-components';
@@ -6,23 +5,46 @@ import { Logos } from './components/Logos';
 import { Intro } from './components/Intro';
 
 const GlobalStyle = createGlobalStyle`
+    body {
+      margin: 0;
+      font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+        'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+        sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+    }
+
+    code {
+      font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+        monospace;
+    }   
+
     p,
     input,
     select,
     button {
       font-size: 1.5rem;
+    } 
+
+    .swiper-slide {
+      text-align: center;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      flex-direction: column;
+      width: 700px;
     }
 `;
 
-export default function App() {
-  const Container = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    flex-wrap: wrap;
-  `;
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  flex-wrap: wrap;
+`;
 
+export default function App() {
   return (
     <>
       <GlobalStyle />
