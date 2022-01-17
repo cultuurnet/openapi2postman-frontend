6b81933844a2b473112e0d7322a2b9e126c8d714
bram <bram.pauwelyn@publiq.be>
2022-01-17 10:00:32 +0100

    API-35 - Move global styles to seperate JS file

--- src/App.js
+++ src/App.js
@@ -1,41 +1,9 @@
 import { Form } from './Form';
 import React, { useState } from 'react';
+import { GlobalStyle } from './styles/GlobalStyle';
 import styled from 'styled-components';
-import { createGlobalStyle } from 'styled-components';
 import { Logos } from './components/Logos';
 import { Intro } from './components/Intro';
-
-const GlobalStyle = createGlobalStyle`
-    body {
-      margin: 0;
-      font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-        'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-        sans-serif;
-      -webkit-font-smoothing: antialiased;
-      -moz-osx-font-smoothing: grayscale;
-    }
-
-    code {
-      font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
-        monospace;
-    }   
-
-    p,
-    input,
-    select,
-    button {
-      font-size: 1.5rem;
-    } 
-
-    .swiper-slide {
-      text-align: center;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      flex-direction: column;
-      width: 700px;
-    }
-`;
 
 const Container = styled.div`
   display: flex;
